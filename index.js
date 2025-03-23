a5927fac47ed29f7f04ea05f3c283237a7ad10fd
Miguel-Lock <miguellock333@gmail.com>
2025-03-23 19:59:11 -0400

    Added saving and loading items from localStorage

--- index.js
+++ index.js
@@ -24,3 +24,24 @@ function validateEmail(myInput) {
         button.disabled = true;
     }
 }
+
+// Loads items from localStorage when the page is loaded
+// If item not in local storage: returns empty string
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('firstName').value = localStorage.getItem('firstName') || '';
+    document.getElementById('confirmationSaint').value = localStorage.getItem('confirmationSaint') || '';
+    document.getElementById('lastName').value = localStorage.getItem('lastName') || '';
+    document.getElementById('email').value = localStorage.getItem('email') || '';
+});
+
+// gets the items in eventform when the submit button is clicked
+document.getElementById('eventForm').addEventListener('submit', function(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // Save the form data to localStorage
+    localStorage.setItem("firstName", this.firstName.value);
+    localStorage.setItem("confirmationSaint", this.confirmationSaint.value);
+    localStorage.setItem("lastName", this.lastName.value);
+    localStorage.setItem("email", this.email.value);   
+});
